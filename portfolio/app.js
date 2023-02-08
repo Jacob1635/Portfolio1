6dfec744d36f59388fef0e68f7e9741bc909b127
Jacobdacoder <J.zagorenko@gmail.com>
2023-02-08 10:13:04 -0800

    updated the script style

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,12 +1,28 @@
-// console.log("corgi carousel connected to app.js!");
-$(() => {
+(() => {
+    const $navBtn = $('.nav_btn')
+    $navBtn.on('click', function () {
+        $('.nav_wraper').css('display', 'block')
+    })
 
 
 
+})
+
+console.log('linked')
 
 
+const $navBtn = $('.nav_btn')
+$navBtn.on('click', function () {
+    $('.nav_wraper').css('display', 'block')
+    $('.nav_wraper').on('click', function () {
+        $('.nav_wraper').css("display", 'none')
+    })
+})
 
-
-
-
-});
+$(window).scroll(function () {
+    if (window.scrollY > 30) {
+        $navBtn.css("opacity", "0.6")
+    } else (
+        $navBtn.css("opacity", "1")
+    )
+})

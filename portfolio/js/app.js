6a38ab80f44035dcf4d1fce3085b5fe245ada7c2
Jacobdacoder <J.zagorenko@gmail.com>
2023-02-09 16:44:05 -0800

    lil modify

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -8,13 +8,4 @@ $(document).ready(function(){
            
         }
     })
-    var typed = new typed("element",{
-        string: ["Jacob Zagorenko","a Software Engineering"],
-        smartBackspace:true,
-        typeSpeed:100,
-        backSpeed:100,
-        loop:true,
-        loopCount:Infinity,
-        smartDelay:1000
-    });
-});
+  
